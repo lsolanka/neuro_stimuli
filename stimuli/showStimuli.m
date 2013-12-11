d5aaaa8e0e546fb11e53268ea72538341fdeedec
paolopuggioni <p.paolo321@gmail.com>
2013-12-11 19:41:43 +0000

    fixed the output file

--- stimuli/showStimuli.m
+++ stimuli/showStimuli.m
@@ -82,17 +82,17 @@ function showStimuli(par)
         % ADD THE TRIGGER HERE - Paolo. (Comment this if it runs on our
         % laptops!
         
-         s = daq.createSession('ni');
-         s.addDigitalChannel('Dev1', 'Port1/Line0:7', 'InputOnly');
-         
-         a=0;
-         
-         while a<1 
-           pause(0.0005);
-           testt=s.inputSingleScan;
-           a=testt(end);
-           
-         end
+%          s = daq.createSession('ni');
+%          s.addDigitalChannel('Dev1', 'Port1/Line0:7', 'InputOnly');
+%          
+%          a=0;
+%          
+%          while a<1 
+%            pause(0.0005);
+%            testt=s.inputSingleScan;
+%            a=testt(end);
+%            
+%          end
         
         % Create individual rectangles for the retinotopy
         screenWidth  = screenRect(3) - screenRect(1)
@@ -194,7 +194,16 @@ function showStimuli(par)
         % directionat each redraw:
         shiftperframe= cyclesPerSecond * p * waitduration;
         
+        timeSeq = [];
+        eventSeqDummy = {};
+        tic;
+        
+        currentrect=0;
         for numRect = [1:nRows*nCols]
+            currentrect=currentrect+1;
+            currentrow=mod(currentrect,nCols) +1;
+            currentcol=currentrect-(currentrow-1)*nRows;
+            
             stop = false;
             %if stop
             %    break;
@@ -210,9 +219,9 @@ function showStimuli(par)
             vblStaticTime = vbl + timeStatic;
 
             % record times and order of presentation of stimuli (for a .txt file)
-            timeSeq = [];
-            eventSeqDummy = {};
-            tic;
+            %timeSeq = [];
+            %eventSeqDummy = {};
+            %tic;
 
             % Animationloop:
             for i = [1:numOrient]
@@ -341,7 +350,7 @@ function showStimuli(par)
              orientEventList{2*i-1} = strcat(num2str(orientList_print(i)),' -Static');
              orientEventList{2*i} = strcat(num2str(orientList_print(i)),' -Drift');
          end
-         orientEventList{2*numOrient+1} = 'End';
+         orientEventList{2*numOrient+1} = sprintf('End');
      else
          orientEventList = cell(1,3*numOrient+1);
          for i =[1:numOrient]
@@ -350,10 +359,13 @@ function showStimuli(par)
              orientEventList{3*i} = strcat(num2str(orientList_print(i)),'\tReverse');
          end
          
-         orientEventList{3*numOrient+1} = 'End';
+         orientEventList{3*numOrient+1} = sprintf('End');
      end
      
      % combine order or orientations with other events
+     
+     orientEventList=repmat(orientEventList,[1,numRect]);
+     
      total = length(eventSeqDummy)+length(orientEventList);
      eventSeq = cell(1,total);
      for i = [1:total]
@@ -380,7 +392,20 @@ function showStimuli(par)
      file_name = cat(2,currentPath,name1,fileSuffix,'.txt');
      fid = fopen (file_name,'w');
      
-     fprintf(fid,'Time(s)\tOrient.(deg)-Type\n');
+     fprintf(fid,'PARAMETERS:\n');
+     if numOrient==1
+        fprintf(fid,'Chronic stimulus\n');   
+        fprintf(fid,'Drift time (s): %.1f, Static time(s): %.1f, Spatial Frequency (cyc/deg):%.2f, Temporal Frequency (cyc/s): %.1f\n',timeDrift,timeStatic,spatFreq,cyclesPerSecond);
+        fprintf(fid,'Chronic panel, orientation (deg): %.0f\n',chronicOrient);
+     else
+     if nRows>1
+        fprintf(fid,'Retinotopy, row x col: %d x %d\n',nRows,nCols);       
+     end
+     fprintf(fid,'Drift time (s): %.1f, Static time(s): %.1f, Spatial Frequency (cyc/deg):%.2f, Temporal Frequency (cyc/s): %.1f\n',timeDrift,timeStatic,spatFreq,cyclesPerSecond);
+     fprintf(fid,'N orient.: %d, Random (1=YES,0=NO): %d, Bidirectional (1=YES,0=NO): %d, Stim. Style (1=sin,0=BW): %d, Gabor (1=YES,0=NO): %d\n', numOrient, randomOrder,biDirectional,stimStyle,gabor);
+     end
+     
+     fprintf(fid,'\nTime(s)\tOrient.(deg)-Type\n');
      
      kk=0;
      while kk<length(timeSeq)
